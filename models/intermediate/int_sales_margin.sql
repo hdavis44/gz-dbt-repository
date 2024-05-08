6982c0c697925b5148764fb0bdcbd37e5c5705e9
Henry Davis <henryo.davis@gmail.com>
2024-05-08 15:46:28 +0000

    renamed macro file

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -7,7 +7,7 @@ SELECT
     ,p.purchase_price
     ,ROUND(s.quantity * p.purchase_price,2) AS purchase_cost
     ,ROUND(s.revenue - (s.quantity * p.purchase_price),2) AS margin
-    ,{{ margin_percent(s.revenue, s.quantity * p.purchase_price) }} AS margin_percent
+    ,{{ margin_percent(s.revenue, s.quantity * p.purchase_price) }}
 FROM {{ref("stg_raw__sales")}} AS s
 LEFT JOIN {{ref("stg_raw__product")}} AS p
     ON s.products_id = p.products_id
